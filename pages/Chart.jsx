53ce494628ea38175e9cc012a797086713b2b77a
**sujan <sujanmoi128@gmail.com>
2022-04-04 07:02:04 +0530

    new

---
+++ pages/Chart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AreaChart from '../components/AreaChart'
+
+const Chart = () => {
+  return (
+    <div>
+         <AreaChart/>
+    </div>
+  )
+}
+
+export default Chart
